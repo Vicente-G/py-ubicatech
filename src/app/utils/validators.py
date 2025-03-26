161eb883424a400faf05a76e3f46fd2b12c3ba6d
Vicente-G <vicentegonsal2000@gmail.com>
2025-03-26 09:59:14 -0300

    feat: Add sanitization and validation utilities

---
+++ src/app/utils/validators.py
@@ -0,0 +1,28 @@
+# Standard Library imports
+
+# Core Flask imports
+
+# Third-party imports
+from pydantic import BaseModel, EmailStr, constr, field_validator
+
+# App imports
+
+
+class AccountValidator(BaseModel):
+    username: constr(min_length=1, max_length=15) = ...  # type: ignore
+    email: EmailStr = ...
+    password: str = ...
+
+    @field_validator("username")
+    def username_valid(self, cls, v):
+        if not v[0].isalpha():
+            raise ValueError("Username must start with a letter")
+        if not v.isalnum():
+            raise ValueError(
+                "Username must contain only letters, numbers, and underscores"
+            )
+        return v
+
+
+class EmailValidator(BaseModel):
+    email: EmailStr
