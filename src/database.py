0065363c4859c9592347c3fc27ab247f8081de9a
Vicente-G <vicentegonsal2000@gmail.com>
2025-03-28 13:03:14 -0300

    fix: Handle None db url for CI tests purpose

--- src/database.py
+++ src/database.py
@@ -2,11 +2,14 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from src.config import PG_URL
 
-engine = create_engine(PG_URL)
+engine = None
+if PG_URL is not None:
+    engine = create_engine(PG_URL)
 
 
 def init_db():
-    SQLModel.metadata.create_all(engine)
+    if engine is not None:
+        SQLModel.metadata.create_all(engine)
 
 
 def get_session():
