eb4a8406a4efedfcfe1c77c4077ac3cd2174ff19
Vicente-G <vicentegonsal2000@gmail.com>
2025-04-09 10:11:13 -0400

    feat: initialize Terraform configuration with all modules

---
+++ infrastructure/locals.tf
@@ -0,0 +1,17 @@
+locals {
+  # Terraform's Backend
+  bucket_name = "ubicatech-tf-state-bucket"
+  table_name  = "ubicatech-tf-state-lock-table"
+
+  # AWS Region
+  aws_region = "us-east-1"
+
+  # AWS ECR
+  ecr_repo_name = "ubicatech-ecr-images-repo"
+
+  # VPC Configuration
+  vpc_cidr             = "10.0.0.0/16"
+  availability_zones   = ["us-east-1a", "us-east-1b"]
+  public_subnet_cidrs  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
